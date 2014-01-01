6566f4c28fbfc01781edc12db6ac13db40af1ad8
takav9 <takav9@gmail.com>
2014-01-01 16:57:09 +0900

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,8 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
- 	it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
 	it { should be_valid }
 
@@ -77,7 +78,7 @@ describe User do
     it { should_not be_valid }
   end
 
- describe "with a password that's too short" do
+	describe "with a password that's too short" do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
@@ -96,5 +97,10 @@ describe User do
       it { should_not eq user_for_invalid_password }
       specify { expect(user_for_invalid_password).to be_false }
     end
-  end  
+  end
+
+	describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end    
 end
